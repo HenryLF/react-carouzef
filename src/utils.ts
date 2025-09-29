fb40e151002a0bdd35aa907942cc49b60c14ddd0
HenryLF <henrylf@protonmail.com>
2025-09-29 15:09:28 +0200

    v1.2.4 change hidden condition from to be a lil' laxer and avoid 'disapering slide'

--- src/utils.ts
+++ src/utils.ts
@@ -66,7 +66,7 @@ export function duplicateChildren(children: ReactNode, itemsPerView: number) {
 
   let childArray = Children.toArray(children);
   const minItems = Math.max(itemsPerView * 2, 3);
-  while (childArray.length < minItems) {
+  while (childArray.length <= minItems) {
     childArray = [...childArray, ...childArray.slice(0, count)];
   }
   return childArray;
@@ -90,7 +90,7 @@ export function getItemPosition(distance: number, perView: number) {
   if (distance === 1) {
     return ItemPosition.NEXT;
   }
-  if (Math.abs(distance) > perView / 2 + 1) {
+  if (Math.abs(distance) > Math.ceil(perView / 2) + 1) {
     return ItemPosition.HIDDEN;
   }
   if (distance < 0) {
